e7f7e104e90c76cf32d48d7dbe0858630bc92908
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-06-30 22:24:52 +0300

    init

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {"name":"Sam",
+       "streetAddress": "11 Broadway"
+
+};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = "12 Broadway";
+ return employee;
+   
+}
+function deleteFromEmployeeByKey(employee, name) {
+
+    const newEmployee = {"name":"Sam"};
+    delete newEmployee[name];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    
+    delete employee[name];
+    return employee;
+  }
